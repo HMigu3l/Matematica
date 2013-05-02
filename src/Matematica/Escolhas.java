9601711b8dfc0410944542e86e44da930c66c0ce
heidy miguel <heidymiguel@outlook.com>
2013-05-02 11:51:01 -0700

    Este programa faz uma pergunta do tipo: Quanto é A x B ?

---
+++ src/Matematica/Escolhas.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Matematica;
+
+/**
+ *
+ * @author windows eight
+ */
+public class Escolhas {
+    
+}
